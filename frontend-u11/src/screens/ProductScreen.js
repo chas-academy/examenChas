10dd01c67f59af34bd37535738284c46412ca708
William Rössler <71815793+willrossler@users.noreply.github.com>
2022-01-13 14:37:10 +0100

    add product fetching to products pages from backend to frontend.

--- frontend-u11/src/screens/ProductScreen.js
+++ frontend-u11/src/screens/ProductScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const params = useParams();
-  const product = products.find((p) => p._id === params.id);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`);
+
+      setProduct(data);
+    };
+
+    fetchProduct();
+
+    console.log();
+  }, []);
 
   return (
     <>
@@ -15,7 +27,7 @@ const ProductScreen = () => {
       </Link>
       <Row>
         <Col md={6}>
-          <Image src={product.image} alt={product.name} />
+          <Image src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={3}>
           <ListGroup variant="flush">
