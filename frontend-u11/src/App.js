c0edfab770412ab30cfbdd7b92f2421041134271
William Rössler <71815793+willrossler@users.noreply.github.com>
2022-04-17 20:38:24 +0200

    Add userlist for admin page

--- frontend-u11/src/App.js
+++ frontend-u11/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 const App = () => {
   return (
@@ -30,6 +31,8 @@ const App = () => {
             <Route path="login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
             <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/admin/userlist" element={<UserListScreen />} />
+
             <Route path="/cart">
               <Route path=":id" element={<CartScreen />} />
               <Route path="" element={<CartScreen />} />
